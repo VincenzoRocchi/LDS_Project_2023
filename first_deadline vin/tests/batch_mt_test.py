f2a40d78d9e433b404daf849922cdf62f85f06ee
vincenzo_rocchi <vincenzo.rocchi@gmail.com>
2023-12-06 12:30:31 +0100

    mt tests

--- first_deadline vin/tests/batch_mt_test.py
+++ first_deadline vin/tests/batch_mt_test.py
@@ -25,23 +25,25 @@ def delete_table_contents(connection, table_name):
         # Close the cursor (connection will be closed outside the function)
         cursor.close()
 
-def process_geography_batch(conn, batch):
-
+def process_gun_batch(batch):
     try:
+        # Create a new connection for each thread
+        conn = pyodbc.connect(conn_str)
         conn.execute('BEGIN TRANSACTION')
 
-        # Insert the batch into the Geography table
+        # Insert the batch into the Gun table
         conn.cursor().executemany('''
-            INSERT INTO Geography (geo_id, latitude, longitude, city, state, continent)
-            VALUES (?, ?, ?, ?, ?, ?)
+            INSERT INTO Gun (gun_id, is_stolen, gun_type)
+            VALUES (?, ?, ?)
         ''', batch)
 
         conn.commit()
+        conn.close()  # Close the connection after successful commit
     except Exception as e:
         print(f"An error occurred during batch insertion: {e}")
         conn.rollback()
-
-def populate_geography_from_csv_batch(file_path, connection, batch_size=100, num_workers=2):
+        
+def populate_gun_from_csv_batch(file_path, connection, batch_size=100, num_workers=2):
 
     # Read data from CSV file
     with open(file_path, 'r') as csvfile:
@@ -52,24 +54,20 @@ def populate_geography_from_csv_batch(file_path, connection, batch_size=100, num
 
         # Extract rows from the CSV file
         rows = [
-            (row[0], row[1], row[2], row[3], row[4], row[5]) for row in csv_reader
+            (row[0], row[1], row[2]) for row in csv_reader
         ]
 
     # Split rows into batches
     batches = [rows[i:i + batch_size] for i in range(0, len(rows), batch_size)]
 
     # Use ThreadPoolExecutor for parallelization
-    with ThreadPoolExecutor(max_workers=num_workers) as executor, tqdm(total=len(rows), desc='Inserting into Geography table') as pbar:
-        for batch in batches:
-            # Process batches in parallel
-            futures = [executor.submit(process_geography_batch, connection, batch) for batch in batches]
+    with ThreadPoolExecutor(max_workers=num_workers) as executor, tqdm(total=len(rows), desc='Inserting into Gun table') as pbar:
+        futures = [executor.submit(process_gun_batch, batch) for batch in batches]
 
-            # Wait for all futures to complete
-            for future in futures:
-                future.result()
-
-                # Update the progress bar
-                pbar.update(len(batch))
+        # Wait for all futures to complete
+        for future in futures:
+            future.result()
+            pbar.update(len(batch))
 
 # Connection data
 server = 'tcp:lds.di.unipi.it'
@@ -81,7 +79,6 @@ database = 'Group_ID_183_DB'
 conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
 conn = pyodbc.connect(conn_str)
-cursor = conn.cursor()
 
 delete_table_contents(conn, 'Geography') # Delete the contents of the table before populating it
-populate_geography_from_csv_batch('DATA/Geography.csv', conn)
+populate_gun_from_csv_batch('DATA/Geography.csv', conn)
